8101901688cade66b9f10b3166acb91979ee7017
Aditya Azad <adityaazad79@gmail.com>
2023-02-25 10:21:01 +0530

    lambda compatiblity

--- application.py
+++ application.py
@@ -9,112 +9,112 @@ import logging
 import requests
 import pymongo
 
+def lambda_handler(event, context):
+    logging.basicConfig(filename="scrapper.log", level=logging.INFO)
 
-logging.basicConfig(filename="scrapper.log", level=logging.INFO)
-
-application = Flask(__name__)
-app = application
-
-
-@app.route('/', methods=['GET'])
-@cross_origin()
-def homePage():
-    return render_template("index.html")
+    application = Flask(__name__)
+    app = application
 
 
-# route to show the review comments in a web UI
-@app.route('/scrape', methods=['POST', 'GET'])
-@cross_origin()
-def index():
-    if request.method == 'POST':
-        try:
-            searchString = request.form['content']
-            options = webdriver.ChromeOptions()
-            options.add_argument("--headless")
-            browser = webdriver.Chrome(
-                options=options, service=Service(ChromeDriverManager().install()))
-            browser.get(searchString)
-            browser.execute_script("window.scrollTo(0,400)", "")
+    @app.route('/', methods=['GET'])
+    @cross_origin()
+    def homePage():
+        return render_template("index.html")
 
-            youtubePage = browser.page_source
-            soup = bs(youtubePage, "html.parser")
 
-            link = []
-            thumbnail = []
-            title = []
-            views = []
-            time = []
-
-            soupData = soup.find("div", {"id": "contents"})
-
-            # Link
+    # route to show the review comments in a web UI
+    @app.route('/scrape', methods=['POST', 'GET'])
+    @cross_origin()
+    def index():
+        if request.method == 'POST':
             try:
-                videoSoup = (soupData.find_all(
-                    "a", {"class": "yt-simple-endpoint inline-block style-scope ytd-thumbnail"}))[0:5]
-                for i in videoSoup:
-                    link.append("https://www.youtube.com"+str(i.get("href")))
-            except:
-                logging.info("Error in Link")
+                searchString = request.form['content']
+                options = webdriver.ChromeOptions()
+                options.add_argument("--headless")
+                browser = webdriver.Chrome(
+                    options=options, service=Service(ChromeDriverManager().install()))
+                browser.get(searchString)
+                browser.execute_script("window.scrollTo(0,400)", "")
 
-            # Thumbnail
-            try:
-                soupvideoThumb = soup.find_all(
-                    "img", {"class": "yt-core-image--fill-parent-height"})[:5]
-                for i in soupvideoThumb:
-                    thumbnail.append(str(i['src']))
-            except:
-                logging.info("Error in Thumbnail Link")
+                youtubePage = browser.page_source
+                soup = bs(youtubePage, "html.parser")
 
-            # Title
-            try:
-                soupTitle = (soupData.find_all("a", {
-                    "class": "yt-simple-endpoint focus-on-expand style-scope ytd-rich-grid-media"}))[0:5]
-                for i in soupTitle:
-                    title.append(str(i.get("title")))
-            except:
-                logging.info("Error in Title")
+                link = []
+                thumbnail = []
+                title = []
+                views = []
+                time = []
 
-            # No of views
-            try:
-                soupViews = (soupData.find_all("div", {"id": "metadata"}))[0:5]
-                for i in soupViews:
-                    views.append(str(i.find_all("span")[1].text))
-            except:
-                logging.info("Error in no of views")
+                soupData = soup.find("div", {"id": "contents"})
 
-            # Time of Posting
-            try:
-                soupTime = (soupData.find_all("div", {"id": "metadata"}))[0:5]
-                for i in soupTime:
-                    time.append(str(i.find_all("span")[2].text))
-            except:
-                logging.info("Error in Time of Posting")
+                # Link
+                try:
+                    videoSoup = (soupData.find_all(
+                        "a", {"class": "yt-simple-endpoint inline-block style-scope ytd-thumbnail"}))[0:5]
+                    for i in videoSoup:
+                        link.append("https://www.youtube.com"+str(i.get("href")))
+                except:
+                    logging.info("Error in Link")
 
-            sc_data = []
-            for i in range(5):
+                # Thumbnail
+                try:
+                    soupvideoThumb = soup.find_all(
+                        "img", {"class": "yt-core-image--fill-parent-height"})[:5]
+                    for i in soupvideoThumb:
+                        thumbnail.append(str(i['src']))
+                except:
+                    logging.info("Error in Thumbnail Link")
 
-                dict = {
-                    "link": link[i],
-                    "thumbnail": thumbnail[i],
-                    "title": title[i],
-                    "views": views[i],
-                    "time": time[i]
-                }
+                # Title
+                try:
+                    soupTitle = (soupData.find_all("a", {
+                        "class": "yt-simple-endpoint focus-on-expand style-scope ytd-rich-grid-media"}))[0:5]
+                    for i in soupTitle:
+                        title.append(str(i.get("title")))
+                except:
+                    logging.info("Error in Title")
 
-                sc_data.append(dict)
-                
-            with open("yt.csv", "w") as f:
-                for i in sc_data:
-                    f.write(str(i)+"\n")
+                # No of views
+                try:
+                    soupViews = (soupData.find_all("div", {"id": "metadata"}))[0:5]
+                    for i in soupViews:
+                        views.append(str(i.find_all("span")[1].text))
+                except:
+                    logging.info("Error in no of views")
 
-            return render_template('results.html', reviews=sc_data)
-        except Exception as e:
-            logging.info(e)
-            return 'something is wrong'
+                # Time of Posting
+                try:
+                    soupTime = (soupData.find_all("div", {"id": "metadata"}))[0:5]
+                    for i in soupTime:
+                        time.append(str(i.find_all("span")[2].text))
+                except:
+                    logging.info("Error in Time of Posting")
 
-    else:
-        return render_template('index.html')
+                sc_data = []
+                for i in range(5):
+
+                    dict = {
+                        "link": link[i],
+                        "thumbnail": thumbnail[i],
+                        "title": title[i],
+                        "views": views[i],
+                        "time": time[i]
+                    }
+
+                    sc_data.append(dict)
+                    
+                with open("yt.csv", "w") as f:
+                    for i in sc_data:
+                        f.write(str(i)+"\n")
+
+                return render_template('results.html', reviews=sc_data)
+            except Exception as e:
+                logging.info(e)
+                return 'something is wrong'
+
+        else:
+            return render_template('index.html')
 
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    if __name__ == "__main__":
+        app.run(host='127.0.0.1', port=8000, debug=True)
